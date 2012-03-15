9adc3461639fc1bca0c3b1718cbd04e9dac888c3
Alex Rodionov <p0deje@gmail.com>
2012-03-15 22:42:55 +0700

    Fixing double slash

--- extension/data/pageupdate.js
+++ extension/data/pageupdate.js
@@ -7,7 +7,7 @@ var tab = window.document.getElementsByClassName('title-actions-bar')[0];
 var h1 = tab.getElementsByTagName('h1')[0];
 var strong = h1.getElementsByTagName('strong')[0];
           
-var project = window.location.pathname.split('/').splice(0,3).join('/');
+var project = window.location.pathname.split('/').splice(0,3).join('/').replace(/\/\//, '/');
 var img = window.document.createElement('img');
             
 img.setAttribute('src', 'https://secure.travis-ci.org' + project + '.png');
